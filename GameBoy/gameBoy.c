e1bd17f60328f66b714271add1de04b6b2a33f0b
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2020-04-14 17:28:00 -0300

    optimizado el codigo, manejado mejor el tema de las conexiones
    ademas agregado un par de cositas para hacer lindo el codigo

--- GameBoy/gameBoy.c
+++ GameBoy/gameBoy.c
@@ -1,48 +1,54 @@
 #include "gameBoy.h"
 
+// DECLARACIONES Y DEMASES EN EL .h
+
 int main(void)
 {
-	int conexionBroker;
-	int conexionTeam;
-	int conexionGameCard;
+	int conexiones[CANTCONEXIONES];
 
+	// LOG DE PRUEBA
 	t_log* logger = log_create("gameBoy.log", "GameBoy", true, LOG_LEVEL_INFO);
 	log_info(logger, "GameBoy!");
 
+	// CREAR CONFIG Y OBTENER VALORES (IP=PUERTO)
 	t_config* config = config_create("gameBoy.config");
-	char* ipBroker = config_get_string_value(config,"IP_BROKER");
-	char* puertoBroker = config_get_string_value(config,"PUERTO_BROKER");
-	char* ipTeam = config_get_string_value(config,"IP_TEAM");
-	char* puertoTeam = config_get_string_value(config,"PUERTO_TEAM");
-	char* ipGameCard = config_get_string_value(config,"IP_GAMECARD");
-	char* puertoGameCard = config_get_string_value(config,"PUERTO_GAMECARD");
+	char* ipBroker = config_get_string_value(config, "IP_BROKER");
+	char* puertoBroker = config_get_string_value(config, "PUERTO_BROKER");
+	char* ipTeam = config_get_string_value(config, "IP_TEAM");
+	char* puertoTeam = config_get_string_value(config, "PUERTO_TEAM");
+	char* ipGameCard = config_get_string_value(config, "IP_GAMECARD");
+	char* puertoGameCard = config_get_string_value(config, "PUERTO_GAMECARD");
 
-	conexionBroker = Socket_Crear(ipBroker,puertoBroker);
-	if (conexionBroker == -1) {
-		log_error(logger, "No se pudo crear la conexion");
-		return 1;
+	// CREAR CONEXIONES
+	conexiones[0] = Socket_Crear(ipBroker, puertoBroker);
+	conexiones[1] = Socket_Crear(ipTeam, puertoTeam);
+	conexiones[2] = Socket_Crear(ipGameCard, puertoGameCard);
+
+	// VERIFICAR CONEXIONES
+	for (int i = 0; i<CANTCONEXIONES; i++) {
+		verificarConexion(conexiones[i], logger);
 	}
-	log_info(logger,"Se conecto al proceso Broker");
 
-	conexionTeam = Socket_Crear(ipTeam,puertoTeam);
-	if (conexionTeam == -1) {
-		log_error(logger, "No se pudo crear la conexion");
-		return 1;
-	}
-	log_info(logger,"Se conecto al proceso Team");
-
-	conexionGameCard = Socket_Crear(ipGameCard,puertoGameCard);
-	if (conexionGameCard == -1) {
-		log_error(logger, "No se pudo crear la conexion");
-		return 1;
-	}
-	log_info(logger,"Se conecto al proceso GameCard");
-
-	Socket_LiberarConexion(conexionBroker);
-	Socket_LiberarConexion(conexionTeam);
-	Socket_LiberarConexion(conexionGameCard);
-	config_destroy(config);
-	log_destroy(logger);
+	// TERMINAR PROGRAMA
+	terminarPrograma(conexiones, logger, config);
 
 	return 0;
 }
+
+void terminarPrograma(int conexiones[], t_log* logger, t_config* config)
+{
+	log_destroy(logger);
+	config_destroy(config);
+	for (int i = 0; i<CANTCONEXIONES; i++) {
+		Socket_LiberarConexion(conexiones[i]);
+	}
+}
+
+void verificarConexion(int conexion, t_log* logger) {
+	if (conexion == -1) {
+			log_error(logger, "No se pudo crear la conexion");
+		} else {
+			log_info(logger, "Se conecto al proceso");
+		}
+}
+
--- GameBoy/gameBoy.h
+++ GameBoy/gameBoy.h
@@ -1,4 +1,8 @@
 #include <commons/log.h>
 #include <commons/config.h>
 #include <commons/collections/list.h>
+#define CANTCONEXIONES 3
+
+void terminarPrograma(int conexiones[], t_log* logger, t_config* config);
+void verificarConexion(int conexion, t_log* logger);
 
