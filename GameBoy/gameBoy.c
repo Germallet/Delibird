107254992a39dcb09d0cbad80ac5ed263271388e
Nicolas Naccachian <nanaccachian@gmail.com>
2020-04-14 16:20:01 -0300

    Ahora esta mejor

--- GameBoy/gameBoy.c
+++ GameBoy/gameBoy.c
@@ -1,9 +1,11 @@
 #include "gameBoy.h"
 
 
-int main()
+int main(void)
 {
-	u_int32_t conexiones[3];
+	int conexionBroker;
+	int conexionTeam;
+	int conexionGameCard;
 
 	t_log* logger = log_create("gameBoy.log", "GameBoy", true, LOG_LEVEL_INFO);
 	log_info(logger, "GameBoy!");
@@ -16,21 +18,15 @@ int main()
 	char* ipGameCard = config_get_string_value(config,"IP_GAMECARD");
 	char* puertoGameCard = config_get_string_value(config,"PUERTO_GAMECARD");
 
-	u_int32_t conexiones[0] = Socket_Crear(ipBroker,puertoBroker);
-	u_int32_t conexiones[1] = Socket_Crear(ipTeam,puertoTeam);
-	u_int32_t conexiones[2] = Socket_Crear(ipGameCard,puertoGameCard);
+	conexionBroker = Socket_Crear(ipBroker,puertoBroker);
+	conexionTeam = Socket_Crear(ipTeam,puertoTeam);
+	conexionGameCard = Socket_Crear(ipGameCard,puertoGameCard);
 
-	terminarPrograma(conexiones,logger,config);
-
-	return 0;
-}
-
-
-void terminarPrograma(u_int32_t conexiones[3], t_log* logger, t_config* config) {
-
-	for (int i=0;i<3;i++) {
-		Socket_LiberarConexion(conexiones[i]);
-	}
+	Socket_LiberarConexion(conexionBroker);
+	Socket_LiberarConexion(conexionTeam);
+	Socket_LiberarConexion(conexionGameCard);
 	config_destroy(config);
 	log_destroy(logger);
+
+	return 0;
 }
--- GameBoy/gameBoy.h
+++ GameBoy/gameBoy.h
@@ -2,5 +2,3 @@
 #include <commons/config.h>
 #include <commons/collections/list.h>
 
-
-void terminarPrograma(u_int32_t conexiones[], t_log* logger, t_config* config) {
