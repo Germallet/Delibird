9c0609d8ddd8eddd2eb9cf14a7faa3bbf757c7e6
Germallet <germanmallet@gmail.com>
2020-04-19 16:25:48 -0300

    Parámetros del main

--- GameBoy/gameBoy.c
+++ GameBoy/gameBoy.c
@@ -2,13 +2,17 @@
 
 // DECLARACIONES Y DEMASES EN EL .h
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int conexiones[CANTCONEXIONES];
 
 	// LOG DE PRUEBA
 	t_log* logger = log_create("gameBoy.log", "GameBoy", true, LOG_LEVEL_INFO);
 	log_info(logger, "GameBoy!");
+
+	log_info(logger, "Parametros: %d", argc);
+	for (int i = 0; i < argc; i++)
+		log_info(logger, "Parametro %d: %s", i, argv[i]);
 
 	// CREAR CONFIG Y OBTENER VALORES (IP=PUERTO)
 	t_config* config = config_create("gameBoy.config");
@@ -51,4 +55,3 @@ void verificarConexion(int conexion, t_log* logger) {
 			log_info(logger, "Se conecto al proceso");
 		}
 }
-
