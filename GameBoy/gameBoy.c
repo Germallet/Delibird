c6339833bb045c3bed7a8d56a9aa5d202c3f892a
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2020-04-19 18:44:55 -0300

    gestion de mensajes iniciado

--- GameBoy/gameBoy.c
+++ GameBoy/gameBoy.c
@@ -1,18 +1,11 @@
 #include "gameBoy.h"
 
-// DECLARACIONES Y DEMASES EN EL .h
+// DECLARACIONES, VARIABLES GLOBALES Y DEMASES EN EL .h
 
 int main(int argc, char *argv[])
 {
 	int conexiones[CANTCONEXIONES];
 
-	// LOG DE PRUEBA
-	t_log* logger = log_create("gameBoy.log", "GameBoy", true, LOG_LEVEL_INFO);
-	log_info(logger, "GameBoy!");
-
-	log_info(logger, "Parametros: %d", argc);
-	for (int i = 0; i < argc; i++)
-		log_info(logger, "Parametro %d: %s", i, argv[i]);
 
 	// CREAR CONFIG Y OBTENER VALORES (IP=PUERTO)
 	t_config* config = config_create("gameBoy.config");
@@ -33,14 +26,26 @@ int main(int argc, char *argv[])
 		verificarConexion(conexiones[i], logger);
 	}
 
+	// GESTION DE MENSAJES
+
+	if (sonIguales(argv[1],"TEAM")) {
+		gestionarProcesoTeam(argv,conexiones[1]);
+	} else if (sonIguales(argv[1],"BROKER")) {
+		gestionarProcesoBroker(argv);
+	} else if (sonIguales(argv[1],"GAMECARD")) {
+		gestionarProcesoGameCard(argv);
+	} else if (sonIguales(argv[1],"SUSCRIPTOR")) {
+		gestionarProcesoSuscriptor(argv);
+	}
+
 	// TERMINAR PROGRAMA
 	terminarPrograma(conexiones, logger, config);
 
 	return 0;
 }
 
-void terminarPrograma(int conexiones[], t_log* logger, t_config* config)
-{
+void terminarPrograma(int conexiones[], t_log* logger, t_config* config) {
+
 	log_destroy(logger);
 	config_destroy(config);
 	for (int i = 0; i<CANTCONEXIONES; i++) {
@@ -49,9 +54,62 @@ void terminarPrograma(int conexiones[], t_log* logger, t_config* config)
 }
 
 void verificarConexion(int conexion, t_log* logger) {
+
 	if (conexion == -1) {
 			log_error(logger, "No se pudo crear la conexion");
 		} else {
 			log_info(logger, "Se conecto al proceso");
 		}
 }
+
+int sonIguales(char* a, char* b) {
+
+	return strcmp(a,b) != 0;
+}
+
+void gestionarProcesoTeam(char* argv[], int numSocket) {
+
+	int r = Socket_Enviar(argv[2],argv,sizeof(argv),numSocket);
+
+	if (r == 0) {
+		log_info(logger, "Se envio APPEARED_POKEMON correctamente");
+	} else {
+		log_error(logger, "No se pudo enviar APPEARED_POKEMON");
+	}
+}
+
+void gestionarProcesoBroker(char* argv[]) {
+
+	if (sonIguales(argv[2], "NEW_POKEMON")) {
+
+	} else if (sonIguales(argv[2], "APPEARED_POKEMON")) {
+
+	} else if (sonIguales(argv[2], "CATCH_POKEMON")) {
+
+	} else if (sonIguales(argv[2], "CAUGHT_POKEMON")) {
+
+	} else if (sonIguales(argv[2], "GET_POKEMON")) {
+
+	}
+}
+
+void gestionarProcesoGameCard(char* argv[]) {
+
+	if (sonIguales(argv[2], "NEW_POKEMON")) {
+
+	} else if (sonIguales(argv[2], "CATCH_POKEMON")) {
+
+	} else if (sonIguales(argv[2], "GET_POKEMON")) {
+
+	}
+}
+
+void gestionarProcesoSuscriptor(char* argv[]) {
+
+}
+
+
+
+
+
+
--- GameBoy/gameBoy.h
+++ GameBoy/gameBoy.h
@@ -1,9 +1,26 @@
+// INCLUDES Y DEFINES
 #include <commons/log.h>
 #include <commons/config.h>
 #include <commons/collections/list.h>
 #include "../Utils/socket.h"
 #define CANTCONEXIONES 3
 
+
+// DECLARACIONES
 void terminarPrograma(int conexiones[], t_log* logger, t_config* config);
 void verificarConexion(int conexion, t_log* logger);
+int sonIguales(char* a, char* b);
+void gestionarProcesoTeam(char* argv[]);
+void gestionarProcesoBroker(char* argv[]);
+void gestionarProcesoGameCard(char* argv[]);
+void gestionarProcesoSuscriptor(char* argv[]);
+
+
+// LOG INICIALIZADO
+t_log* logger = log_create("gameBoy.log", "GameBoy", true, LOG_LEVEL_INFO);
+log_info(logger, "Se inicio el proceso GameBoy!");
+
+
+
+
 
