3a2cf1e81807d08a4cf4214e4ccf9f4c39385c05
Germán Mallet <germanmallet@gmail.com>
2020-04-22 12:17:27 -0300

    Removido logger global

--- Utils/logger.h
+++
@@ -1,7 +0,0 @@
-#include <commons/log.h>
-
-void Log_Inicializar(char* nombreArchivo, char* nombrePrograma, bool mostrarEnConsola, t_log_level nivel);
-void Log_Finalizar();
-void Log(char* mensaje);
-void Log_Warning(char* mensaje);
-void Log_Error(char* mensaje);
