f00a35a146fe1aa515c4fac9bc20348852b2801a
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2020-05-30 19:06:48 -0300

    socket de escucha medio hecho

--- GameCard/gameCard.c
+++ GameCard/gameCard.c
@@ -1,46 +1,36 @@
 #include "gameCard.h"
+#include "mensajes.h"
 
-
-t_log* logger;
 t_config* config;
 
 int main()
 {
 	logger = log_create("gameCard.log", "GameCard", true, LOG_LEVEL_INFO);
-	config = config_create("gameBoy.config");
+	config = config_create("gameCard.config");
 	log_info(logger, "Bienvenido a la GameCard!");
+	char* miIp = config_get_string_value(config,"IP_GAMECARD");
+	int miPuerto = config_get_int_value(config,"PUERTO_GAMECARD");
 
-	char* puntoMontaje = config_get_string_value(config,"PUNTO_MONTAJE_TALLGRASS");
-	int tiempoReintentoConexion = config_get_int_value(config,"TIEMPO_DE_REINTENTO_CONEXION");
-	int tiempoReintentoOperacion = config_get_int_value(config,"TIEMPO_DE_REINTENTO_OPERACION");
-	int tiempoRetardoOperacion = config_get_int_value(config,"TIEMPO_RETARDO_OPERACION");
-	char* ipBroker = config_get_int_value(config,"IP_BROKER");
-	int puertoBroker = config_get_int_value(config,"PUERTO_BROKER");
+//	char* puntoMontaje = config_get_string_value(config,"PUNTO_MONTAJE_TALLGRASS");
+//	int tiempoReintentoConexion = config_get_int_value(config,"TIEMPO_DE_REINTENTO_CONEXION");
+//	int tiempoReintentoOperacion = config_get_int_value(config,"TIEMPO_DE_REINTENTO_OPERACION");
+//	int tiempoRetardoOperacion = config_get_int_value(config,"TIEMPO_RETARDO_OPERACION");
+//	char* ipBroker = config_get_string_value(config,"IP_BROKER");
+//	int puertoBroker = config_get_int_value(config,"PUERTO_BROKER");
+//
+//	Eventos* eventos = Eventos_Crear0();
 
+//	Cliente clienteBroker = CrearCliente(ipBroker,puertoBroker,eventos);
 
-	SuscribirseColas(ipBroker,puertoBroker);
+//	SuscribirseColas(clienteBroker);
+
+	SocketEscucha(miIp, miPuerto);
+
+	//EN ALGUN LADO METER UN FORK PARA TENER VARIOS PROCESO GAME CARD
 
 	TerminarPrograma(logger,config);
 	return 0;
 }
-
-
-void SuscribirseColas(char* ip,int puerto) {
-
-}
-
-//void EnviarID(Cliente* cliente, uint32_t identificador)
-//{
-//	DATOS_ID_MENSAJE* id_mensaje = malloc(sizeof(DATOS_ID_MENSAJE));
-//
-//	id_mensaje->id = identificador;
-//	log_error(logger, "Enviando ACK %d", id_mensaje->id);
-//	EnviarMensaje(cliente, BROKER_ACK, id_mensaje, (void*) &SerializarM_ID_MENSAJE);
-//
-//	free(id_mensaje);
-//
-//}
-
 
 void TerminarProgramaConError(char* error)
 {
--- GameCard/gameCard.h
+++ GameCard/gameCard.h
@@ -3,11 +3,12 @@
 #include <commons/collections/list.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <fuse.h>
+//#include <fuse.h>
 #include <stdint.h>
 #include "../Utils/socket.h"
-#include "mensajes.h"
 //#include "../Utils/protocolo.h"
+
+t_log* logger;
 
 typedef struct{
 	char directory;
@@ -18,7 +19,9 @@ typedef struct {
 	uint32_t size;
 	uint32_t* blocks;
 	char open;
-}DatosArchivosPokemon;
+} DatosArchivosPokemon;
 
+void informar(char* log);
+void informarError(char* error);
 void TerminarProgramaConError(char* error);
 void TerminarPrograma(t_log* logger, t_config* config);
