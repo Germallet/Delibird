bee79b07039baa451f0bcb2279c6e108e41ee74b
Germallet <germanmallet@gmail.com>
2020-07-14 15:27:07 -0300

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+echo "Deploying Delibird"
+CWD=$PWD
+
+COMMONS="so-commons-library"
+git clone "https://github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
+cd $COMMONS
+sudo make uninstall
+make all
+sudo make install
+cd $CWD
+
+make all
